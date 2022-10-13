9c7018b3d1146d9128b1ca1ac4af12f4979ee855
Alez2312 <alejandropl2509@gmail.com>
2022-10-13 12:54:45 -0500

    worked v1.0 from

--- Pagina-Web/login.js
+++ Pagina-Web/login.js
@@ -36,5 +36,11 @@
             return true;
         }
     }
+
+    function menuOnClick() {
+        document.getElementById("menu-bar").classList.toggle("change");
+        document.getElementById("nav").classList.toggle("change");
+        document.getElementById("menu-bg").classList.toggle("change-bg");
+      }
     
     
